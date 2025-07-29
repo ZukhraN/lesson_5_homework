45b8903256107c3652c5ec1c1cddd58c8ecc0ebf
znurmanova@dar.llp <zukhra.nurmanova@gmail.com>
2025-07-29 17:26:30 +0500

    homework 1st part

---
+++ src/test/java/ui/homeWorkTests.java
@@ -0,0 +1,71 @@
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class homeWorkTests {
+
+    private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.close();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testFile.csv", numLinesToSkip = 1)
+    void checkAllElementsOnPageTest(String nameOfLabel){
+        List<WebElement> inputFields = driver. findElements(By.xpath("//label[normalize-space(text())='"+ nameOfLabel +"']/input"));
+        for (WebElement inputField: inputFields){
+            inputField.click();
+        }
+    }
+
+    @Test
+    void fillAllElementsTest() throws InterruptedException{
+        //поле Text input
+        WebElement inputTextField = driver.findElement(By.xpath("//label[normalize-space(text())='Text input']/input"));
+        inputTextField.click();
+        inputTextField.sendKeys("testText");
+        WebElement titleChapter = driver.findElement(By.cssSelector(".display-6"));
+        titleChapter.click();
+
+        Assertions.assertEquals("testText", inputTextField.getAttribute("value"));
+
+        //поле Password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@type='password']"));
+        inputPassword.click();
+        inputPassword.sendKeys("test");
+        titleChapter.click();
+
+        Assertions.assertEquals("test", inputPassword.getAttribute("value"));
+
+        //поле Textarea
+        WebElement textareaText = driver.findElement(By.xpath("//textarea[@name='my-textarea']"));
+        textareaText.click();
+        textareaText.sendKeys("test");
+        titleChapter.click();
+
+        Assertions.assertEquals("test", textareaText.getAttribute("value"));
+
+
+    }
+}
