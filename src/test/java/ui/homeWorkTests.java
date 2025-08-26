721c98dc58f5442497e9a79acf3b4fc921d2c1bb
znurmanova@dar.llp <zukhra.nurmanova@gmail.com>
2025-08-26 17:05:42 +0500

    homework final part

--- src/test/java/ui/homeWorkTests.java
+++ src/test/java/ui/homeWorkTests.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
 import java.util.List;
 
 public class homeWorkTests {
@@ -84,13 +87,70 @@ public class homeWorkTests {
 
         Assertions.assertEquals("One", optionDropDown.getText());
 
-        //поле Dropdown (select)
+        //поле Dropdown (datalist)
         WebElement dropDownDataList = driver.findElement(By.xpath("//input[@name='my-datalist']"));
-        dropDownDataList.click();
-        WebElement optionDataList = driver.findElement(By.xpath("//datalist/option[@value='New York']"));
-        //optionDataList.click();
+        dropDownDataList.sendKeys("New York");
 
+        Assertions.assertEquals("New York", dropDownDataList.getAttribute("value"));
+
+        //поле File input
+        WebElement fileInput = driver.findElement(By.xpath("//input[@name='my-file']"));
+        fileInput.sendKeys(new File("src/test/resources/pic.png").getAbsolutePath());
+        String value = fileInput.getAttribute("value");
+        String fileName = value.substring(value.lastIndexOf("\\") + 1);
+
+        Assertions.assertEquals("pic.png", fileName);
+
+
+        //поля Check boxes
+        WebElement checkBox1 = driver.findElement(By.xpath("//div[@class='form-check']//input[@name='my-check' and not(@checked)]"));
+        checkBox1.click();
         //Assertions.assertEquals("New York", optionDataList.getAttribute("value"));
+
+        //поля Radio button
+        WebElement radioButton = driver.findElement(By.xpath("//input[@name='my-radio' and not(@checked)]"));
+        radioButton.click();
+        //Assertions.assertEquals("New York", optionDataList.getAttribute("value"));
+
+        // Color picker
+        WebElement colorPicker = driver.findElement(By.xpath("//div//input[@name='my-colors' ]"));
+        colorPicker.click();
+        colorPicker.sendKeys("#198754");
+
+        Assertions.assertEquals("#198754", colorPicker.getAttribute("value"));
+
+        // Date picker
+        WebElement datePicker = driver.findElement(By.xpath("//div//input[@name='my-date']"));
+        datePicker.click();
+        datePicker.sendKeys("08/27/2025");
+
+        Assertions.assertEquals("08/27/2025", datePicker.getAttribute("value"));
+
+        // Example range
+        WebElement range = driver.findElement(By.name("my-range"));
+        int target = 7;
+
+        int current = Integer.parseInt(range.getAttribute("value"));
+        while (current < target) {
+            range.sendKeys(Keys.ARROW_RIGHT);
+            current = Integer.parseInt(range.getAttribute("value"));
+        }
+        while (current > target) {
+            range.sendKeys(Keys.ARROW_LEFT);
+            current = Integer.parseInt(range.getAttribute("value"));
+        }
+
+        Assertions.assertEquals(target,
+                Integer.parseInt(range.getAttribute("value")),
+                "Слайдер не встал на нужное значение");
+
+
+        //Send form
+        WebElement submitButton = driver.findElement(By.xpath("//div//button[@type='submit']"));
+        submitButton.click();
+        WebElement message = driver.findElement(By.className("display-6"));
+
+        Assertions.assertEquals("Form submitted", message.getText(), "Сообщение после сабмита не совпало");
 
     }
 }
